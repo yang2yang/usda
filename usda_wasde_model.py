acb2804042771a972c8e96e4cfce59f735d95b0d
jack <197753119@qq.com>
2016-12-02 18:27:59 +0800

    usda_wasde的model先弄好

---
+++ usda_wasde_model.py
@@ -0,0 +1,130 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Date, Float
+from sqlalchemy.orm import Session
+
+Base = declarative_base()
+
+
+class UsdaWasdeCorn(Base):
+    __tablename__ = 'usda_wasde_corn'
+
+    PublicationDate = Column(Date, primary_key=True)
+    MarketingYear = Column(String, primary_key=True)
+    Stage = Column(String)
+    AreaPlanted = Column(Float)
+    AreaHarvested = Column(Float)
+    Yield = Column(Float)
+    AreaPlanted = Column(Float)
+    BeginningStocks = Column(Float)
+    Production = Column(Float)
+    Imports = Column(Float)
+    TotalSupply = Column(Float)
+    FeedandResidual = Column(Float)
+    FoodSeedIndustrial = Column(Float)
+    EthanolByProducts = Column(Float)
+    TotalDomestic = Column(Float)
+    Exports = Column(Float)
+    TotalUse = Column(Float)
+    EndingStocks = Column(Float)
+    AvgFarmPriceLow = Column(Float)
+    AvgFarmPriceHigh = Column(Float)
+
+
+class usdaWasdeSoybean(Base):
+    __tablename__ = "usda_wasde_soybean"
+
+    PublicationDate = Column(Date, primary_key=True)
+    MarketingYear = Column(String, primary_key=True)
+    Stage = Column(String)
+    AreaPlanted = Column(Float)
+    AreaHarvested = Column(Float)
+    Yield = Column(Float)
+    BeginningStocks = Column(Float)
+    Production = Column(Float)
+    Imports = Column(Float)
+    TotalSupply = Column(Float)
+    FeedandResidual = Column(Float)
+    FoodSeedIndustrial = Column(Float)
+    EthanolByProducts = Column(Float)
+    TotalDomestic = Column(Float)
+    Exports = Column(Float)
+    TotalUse = Column(Float)
+    EndingStocks = Column(Float)
+    AvgFarmPriceLow = Column(Float)
+    AvgFarmPriceHigh = Column(Float)
+
+
+class usdaWasdeSoybeanMeal(Base):
+    __tablename__ = "usda_wasde_soybeanmeal"
+
+    PublicationDate = Column(Date, primary_key=True)
+    MarketingYear = Column(String, primary_key=True)
+    Stage = Column(String)
+    BeginningStocks = Column(Float)
+    Production = Column(Float)
+    Imports = Column(Float)
+    TotalSupply = Column(Float)
+    DomesticDisappearance = Column(Float)
+    Exports = Column(Float)
+    TotalUse = Column(Float)
+    EndingStocks = Column(Float)
+    AvgPriceLow = Column(Float)
+    AvgPriceHigh = Column(Float)
+
+
+class usdaWasdeSoybeanNoil(Base):
+    __tablename__ = "usda_wasde_soybeanoil"
+
+    PublicationDate = Column(Date, primary_key=True)
+    MarketingYear = Column(String, primary_key=True)
+    Stage = Column(String)
+    BeginningStocks = Column(Float)
+    Production = Column(Float)
+    Imports = Column(Float)
+    TotalSupply = Column(Float)
+    DomesticDisappearance = Column(Float)
+    Biodiesel = Column(Float)
+    FoodFeedIndustrial = Column(Float)
+    Exports = Column(Float)
+    TotalUse = Column(Float)
+    EndingStocks = Column(Float)
+    AvgPriceLow = Column(Float)
+    AvgPriceHigh = Column(Float)
+
+
+class usdaWasdeWorldCorn(Base):
+    __tablename__ = "usda_wasde_world_corn"
+
+    PublicationDate = Column(Date, primary_key=True)
+    MarketingYear = Column(String, primary_key=True)
+    Stage = Column(String)
+    Country = Column(String, primary_key=True)
+    BeginningStocks = Column(Float)
+    Production = Column(Float)
+    Imports = Column(Float)
+    DomesticCrush = Column(Float)
+    TotalDomestic = Column(Float)
+    Exports = Column(Float)
+    EndingStocks = Column(Float)
+
+
+class usdaWasdeWorldSoybean(Base):
+    __tablename__ = "usda_wasde_world_soybean"
+
+    PublicationDate = Column(Date, primary_key=True)
+    MarketingYear = Column(String, primary_key=True)
+    Stage = Column(String)
+    Country = Column(String, primary_key=True)
+    BeginningStocks = Column(Float)
+    Production = Column(Float)
+    Imports = Column(Float)
+    DomesticFeed = Column(Float)
+    TotalDomestic = Column(Float)
+    Exports = Column(Float)
+    EndingStocks = Column(Float)
+
+
+engine = create_engine('mysql+pymysql://root:root123@192.168.5.12:3306/data_analyze?charset=utf8', echo=True)
+
+session = Session()
